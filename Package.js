ee4c4140df6ed444930f1ad6d59448346eb1f8f7
yourname202 <chantalmbayo121@gmail.com>
2025-12-22 18:51:12 +0200

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,13 @@
+{
+  "name": "matrix-md",
+  "version": "1.0.0",
+  "description": "Bot WhatsApp Multi-Device - MATRIX",
+  "main": "main.js",
+  "scripts": {
+    "start": "node main.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.0",
+    "pino": "^8.0.0"
+  }
+}
