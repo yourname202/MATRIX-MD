f703524d91a42aa5259f251cb0206c11c8bdf4b1
yourname202 <chantalmbayo121@gmail.com>
2025-12-22 23:57:46 +0200

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,37 @@
+const settings = {
+  packname: 'MATRIX MD',
+  author: 'YOUR NAME-TECH',
+  botName: "MATRIX MD",
+  botOwner: 'YOUR NAME-TECH',
+  phoneNumber: "243982355070",
+  ownerNumber: '243982355070',
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20,
+  storeWriteInterval: 10000,
+  description: "MATRIX MD — Bot WhatsApp en français, rapide, multi-device, créer par YOUR NAME-TECH.",
+  version: "1.0.1",
+  channelLink: "https://whatsapp.com/channel/0029VbCB5Vu6xCSXNqDV9X3v",
+  updateZipUrl: "https://github.com/STIVE-DEVX/ZENTROX-MD-main.zip",
+  botPreview: "https://i.postimg.cc/Dz3mbLwc/bot-image-f-Of28PVl.jpg",
+
+  // Carte globale
+  adReply: {
+    title: "MATRIX BOT MD",
+    body: "BY YOUR NAME TECH",
+    thumbnailUrl: 'https://i.postimg.cc/c4KWPt69/chatgpt-image-3-dec-2025-17-48-55-PQCmcx-Vn.jpg',
+    sourceUrl: 'https://whatsapp.com/channel/0029Vb6nKuV8vd1M1iBlWe2l',
+    mediaType: 1,
+    mediaUrl: 'https://whatsapp.com/channel/0029Vb6nKuV8vd1M1iBlWe2l',
+    renderLargerThumbnail: false
+  },
+
+  // Newsletter global (optionnel, utilisable dans toutes les commandes)
+  newsletter: {
+    newsletterJid: '120363401545196668@newsletter',
+    newsletterName: 'MATRIX MD',
+    serverMessageId: -1
+  }
+};
+
+module.exports = settings;
